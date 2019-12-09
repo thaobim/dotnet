9c5285b381cfb3d4204223610fcb9f27752dab46
thangdangblog <34571494+thangdangblog@users.noreply.github.com>
2019-12-09 09:43:08 +0700

    update

--- QuanlybanHang-MITComputer/frmMain.Designer.cs
+++ QuanlybanHang-MITComputer/frmMain.Designer.cs
@@ -33,28 +33,27 @@
             this.menuQuanli = new System.Windows.Forms.ToolStripMenuItem();
             this.danhMụcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuDoiMatKhau = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuTaikhoan = new System.Windows.Forms.ToolStripMenuItem();
             this.menuLogout = new System.Windows.Forms.ToolStripMenuItem();
             this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.lblKhachhang = new System.Windows.Forms.Label();
-            this.lblNumberKH = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.lblDanhmuc = new System.Windows.Forms.Label();
-            this.lblNumberDM = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.lblNumberSP = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.lblSanpham = new System.Windows.Forms.Label();
+            this.lblNumberSP = new System.Windows.Forms.Label();
+            this.lblNumberDM = new System.Windows.Forms.Label();
+            this.lblNumberKH = new System.Windows.Forms.Label();
+            this.lblKhachhang = new System.Windows.Forms.Label();
+            this.lblDanhmuc = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.bánHàngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -64,7 +63,6 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuDashboard,
-            this.bánHàngToolStripMenuItem,
             this.menuQuanli,
             this.danhMụcToolStripMenuItem,
             this.tàiKhoảnToolStripMenuItem,
@@ -96,25 +94,23 @@
             // tàiKhoảnToolStripMenuItem
             // 
             this.tàiKhoảnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.menuDoiMatKhau,
+            this.menuTaikhoan,
             this.menuLogout});
             this.tàiKhoảnToolStripMenuItem.Name = "tàiKhoảnToolStripMenuItem";
             this.tàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
             this.tàiKhoảnToolStripMenuItem.Text = "Tài khoản";
             // 
-            // menuDoiMatKhau
+            // menuTaikhoan
             // 
-            this.menuDoiMatKhau.Name = "menuDoiMatKhau";
-            this.menuDoiMatKhau.Size = new System.Drawing.Size(180, 22);
-            this.menuDoiMatKhau.Text = "Đổi mật khẩu";
-            this.menuDoiMatKhau.Click += new System.EventHandler(this.MenuDoiMatKhau_Click);
+            this.menuTaikhoan.Name = "menuTaikhoan";
+            this.menuTaikhoan.Size = new System.Drawing.Size(180, 22);
+            this.menuTaikhoan.Text = "Thông tin tài khoản";
             // 
             // menuLogout
             // 
             this.menuLogout.Name = "menuLogout";
             this.menuLogout.Size = new System.Drawing.Size(180, 22);
             this.menuLogout.Text = "Đăng xuất";
-            this.menuLogout.Click += new System.EventHandler(this.MenuLogout_Click);
             // 
             // menuHelp
             // 
@@ -133,69 +129,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 426);
             this.panel1.TabIndex = 1;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.Controls.Add(this.lblKhachhang);
-            this.panel4.Controls.Add(this.lblNumberKH);
-            this.panel4.Controls.Add(this.pictureBox3);
-            this.panel4.Location = new System.Drawing.Point(541, 75);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(238, 120);
-            this.panel4.TabIndex = 1;
-            // 
-            // lblKhachhang
-            // 
-            this.lblKhachhang.AutoSize = true;
-            this.lblKhachhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblKhachhang.Location = new System.Drawing.Point(14, 77);
-            this.lblKhachhang.Name = "lblKhachhang";
-            this.lblKhachhang.Size = new System.Drawing.Size(137, 25);
-            this.lblKhachhang.TabIndex = 4;
-            this.lblKhachhang.Text = "Khách hàng";
-            // 
-            // lblNumberKH
-            // 
-            this.lblNumberKH.AutoSize = true;
-            this.lblNumberKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumberKH.Location = new System.Drawing.Point(43, 21);
-            this.lblNumberKH.Name = "lblNumberKH";
-            this.lblNumberKH.Size = new System.Drawing.Size(57, 39);
-            this.lblNumberKH.TabIndex = 4;
-            this.lblNumberKH.Text = "24";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Controls.Add(this.lblDanhmuc);
-            this.panel3.Controls.Add(this.lblNumberDM);
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Location = new System.Drawing.Point(286, 75);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(238, 120);
-            this.panel3.TabIndex = 1;
-            // 
-            // lblDanhmuc
-            // 
-            this.lblDanhmuc.AutoSize = true;
-            this.lblDanhmuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDanhmuc.Location = new System.Drawing.Point(9, 73);
-            this.lblDanhmuc.Name = "lblDanhmuc";
-            this.lblDanhmuc.Size = new System.Drawing.Size(117, 25);
-            this.lblDanhmuc.TabIndex = 5;
-            this.lblDanhmuc.Text = "Danh mục";
-            // 
-            // lblNumberDM
-            // 
-            this.lblNumberDM.AutoSize = true;
-            this.lblNumberDM.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumberDM.Location = new System.Drawing.Point(41, 21);
-            this.lblNumberDM.Name = "lblNumberDM";
-            this.lblNumberDM.Size = new System.Drawing.Size(57, 39);
-            this.lblNumberDM.TabIndex = 3;
-            this.lblNumberDM.Text = "24";
             // 
             // panel2
             // 
@@ -208,15 +141,27 @@
             this.panel2.Size = new System.Drawing.Size(238, 120);
             this.panel2.TabIndex = 0;
             // 
-            // lblNumberSP
+            // panel3
             // 
-            this.lblNumberSP.AutoSize = true;
-            this.lblNumberSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumberSP.Location = new System.Drawing.Point(52, 21);
-            this.lblNumberSP.Name = "lblNumberSP";
-            this.lblNumberSP.Size = new System.Drawing.Size(57, 39);
-            this.lblNumberSP.TabIndex = 2;
-            this.lblNumberSP.Text = "24";
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.lblDanhmuc);
+            this.panel3.Controls.Add(this.lblNumberDM);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Location = new System.Drawing.Point(286, 75);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(238, 120);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.lblKhachhang);
+            this.panel4.Controls.Add(this.lblNumberKH);
+            this.panel4.Controls.Add(this.pictureBox3);
+            this.panel4.Location = new System.Drawing.Point(541, 75);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(238, 120);
+            this.panel4.TabIndex = 1;
             // 
             // lblSanpham
             // 
@@ -227,6 +172,56 @@
             this.lblSanpham.Size = new System.Drawing.Size(117, 25);
             this.lblSanpham.TabIndex = 1;
             this.lblSanpham.Text = "Sản phẩm";
+            // 
+            // lblNumberSP
+            // 
+            this.lblNumberSP.AutoSize = true;
+            this.lblNumberSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberSP.Location = new System.Drawing.Point(52, 21);
+            this.lblNumberSP.Name = "lblNumberSP";
+            this.lblNumberSP.Size = new System.Drawing.Size(57, 39);
+            this.lblNumberSP.TabIndex = 2;
+            this.lblNumberSP.Text = "24";
+            // 
+            // lblNumberDM
+            // 
+            this.lblNumberDM.AutoSize = true;
+            this.lblNumberDM.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberDM.Location = new System.Drawing.Point(41, 21);
+            this.lblNumberDM.Name = "lblNumberDM";
+            this.lblNumberDM.Size = new System.Drawing.Size(57, 39);
+            this.lblNumberDM.TabIndex = 3;
+            this.lblNumberDM.Text = "24";
+            // 
+            // lblNumberKH
+            // 
+            this.lblNumberKH.AutoSize = true;
+            this.lblNumberKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberKH.Location = new System.Drawing.Point(43, 21);
+            this.lblNumberKH.Name = "lblNumberKH";
+            this.lblNumberKH.Size = new System.Drawing.Size(57, 39);
+            this.lblNumberKH.TabIndex = 4;
+            this.lblNumberKH.Text = "24";
+            // 
+            // lblKhachhang
+            // 
+            this.lblKhachhang.AutoSize = true;
+            this.lblKhachhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKhachhang.Location = new System.Drawing.Point(14, 77);
+            this.lblKhachhang.Name = "lblKhachhang";
+            this.lblKhachhang.Size = new System.Drawing.Size(137, 25);
+            this.lblKhachhang.TabIndex = 4;
+            this.lblKhachhang.Text = "Khách hàng";
+            // 
+            // lblDanhmuc
+            // 
+            this.lblDanhmuc.AutoSize = true;
+            this.lblDanhmuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDanhmuc.Location = new System.Drawing.Point(9, 73);
+            this.lblDanhmuc.Name = "lblDanhmuc";
+            this.lblDanhmuc.Size = new System.Drawing.Size(117, 25);
+            this.lblDanhmuc.TabIndex = 5;
+            this.lblDanhmuc.Text = "Danh mục";
             // 
             // pictureBox3
             // 
@@ -258,13 +253,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // bánHàngToolStripMenuItem
-            // 
-            this.bánHàngToolStripMenuItem.Name = "bánHàngToolStripMenuItem";
-            this.bánHàngToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
-            this.bánHàngToolStripMenuItem.Text = "Bán hàng";
-            this.bánHàngToolStripMenuItem.Click += new System.EventHandler(this.BánHàngToolStripMenuItem_Click);
-            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -278,12 +266,12 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -299,7 +287,7 @@
         private System.Windows.Forms.ToolStripMenuItem menuQuanli;
         private System.Windows.Forms.ToolStripMenuItem danhMụcToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tàiKhoảnToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem menuDoiMatKhau;
+        private System.Windows.Forms.ToolStripMenuItem menuTaikhoan;
         private System.Windows.Forms.ToolStripMenuItem menuLogout;
         private System.Windows.Forms.ToolStripMenuItem menuHelp;
         private System.Windows.Forms.Panel panel1;
@@ -315,7 +303,6 @@
         private System.Windows.Forms.Label lblNumberDM;
         private System.Windows.Forms.Label lblNumberSP;
         private System.Windows.Forms.Label lblSanpham;
-        private System.Windows.Forms.ToolStripMenuItem bánHàngToolStripMenuItem;
     }
 }
 
